37dfba334f202b415ac79d38db961e96d138565f
Matheus Barbosa <ps.matheusb@gmail.com>
2023-06-24 15:12:57 -0300

    configurando Vite e TailWind 1

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import lettering_logo_v from '/lettering_logo_v.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,12 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="#" target="_blank">
-          <img src={lettering_logo_v} className="logo" alt="Stock.io logo" width={400}
-         />
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Stock.io</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -22,6 +25,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
---
+++ web/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    fontFamily: {
+      sans:['Montserrat','Roboto','sans-serif']
+    },
+    extend: {
+      colors:{},
+      backgroundImage:{}
+    },
+  },
+  plugins: [],
+}
+
