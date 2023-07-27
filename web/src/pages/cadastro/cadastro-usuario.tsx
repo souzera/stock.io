1997aecb83343082aa5c0109529eb5d649c4fe45
unknown <towork.matheusb@gmail.com>
2023-07-27 13:24:00 -0300

    tela cadastro

--- web/src/pages/cadastro/cadastro-usuario.tsx
+++ web/src/pages/cadastro/cadastro-usuario.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Space } from "antd";
 
 import { FaIdCardAlt } from 'react-icons/fa'
 import { MdAlternateEmail } from 'react-icons/md'
@@ -11,8 +11,13 @@ export function CadastroUsuario() {
 
 
                 <main className="bg-zinc-100 w-[30%] flex flex-col justify-center items-center px-5 py-12">
+                    <div className="my-4 px-4 w-[90%]">
+                        <a href="/">
+                            <img className="h-full w-full" src="/src/assets/images/SVG/stock_io_logo_name_h.svg" alt="logotipo" />
+                        </a>
+                    </div>
 
-                    <div id="formulario-login" className="flex flex-1 flex-col items-center gap-3">
+                    <div id="formulario-cadastro" className="flex flex-1 flex-col items-center gap-3">
 
                         <div className="">
                             <h2 className="text-2xl font-bold ">
@@ -23,9 +28,9 @@ export function CadastroUsuario() {
                             </h4>
                         </div>
 
-                        <div>
-
+                        <div className="w-[80%]">
                             <Form
+                                className="flex flex-1 flex-col"
                             >
 
                                 <span className="text-base text-zinc-300 mb-1 font-semibold">Nome Completo</span>
@@ -65,43 +70,50 @@ export function CadastroUsuario() {
                                     />
                                 </Form.Item>
 
-                                <span className="text-base text-zinc-300 mb-1 font-semibold">Senha</span>
-                                <Form.Item
-                                    name="password"
-                                    id="password"
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message: 'Digite uma senha'
-                                        },
-                                        //TODO: verificar se o username está sendo usado
-                                    ]}
-                                >
-                                    <Input
-                                        prefix={<IoIosKey className='text-zinc-300 text-xl' />}
-                                        placeholder="senha"
-                                        size="large"
-                                    />
-                                </Form.Item>
+                                <div className="flex gap-x-4">
+                                    <div>
+                                        <span className="text-base text-zinc-300 mb-1 font-semibold">Senha</span>
+                                        <Form.Item
+                                            name="password"
+                                            id="password"
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: 'Digite uma senha'
+                                                },
+                                                //TODO: verificar se o username está sendo usado
+                                            ]}
+                                        >
+                                            <Input
+                                                prefix={<IoIosKey className='text-zinc-300 text-xl' />}
+                                                placeholder="senha"
+                                                size="large"
+                                            />
+                                        </Form.Item>
+                                    </div>
 
-                                <span className="text-base text-zinc-300 mb-1 font-semibold">Confirmar senha</span>
-                                <Form.Item
-                                    name="confirm-password"
-                                    id="confirm-password"
-                                    rules={[
-                                        {
-                                            required: true,
-                                            message: 'Digite uma senha'
-                                        },
-                                        //TODO: verificar se o username está sendo usado
-                                    ]}
-                                >
-                                    <Input
-                                        prefix={<IoIosKey className='text-zinc-300 text-xl' />}
-                                        placeholder="confirmar senha"
-                                        size="large"
-                                    />
-                                </Form.Item>
+                                    <div>
+
+                                        <span className="text-base text-zinc-300 mb-1 font-semibold">Confirmar senha</span>
+                                        <Form.Item
+                                            name="confirm-password"
+                                            id="confirm-password"
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: 'Digite uma senha'
+                                                },
+                                                //TODO: verificar se o username está sendo usado
+                                            ]}
+                                        >
+                                            <Input
+                                                prefix={<IoIosKey className='text-zinc-300 text-xl' />}
+                                                placeholder="confirmar senha"
+                                                size="large"
+                                            />
+                                        </Form.Item>
+                                    </div>
+                                </div>
                                 <Form.Item className="flex justify-center items-center">
 
                                     <Button className="bg-purple-500 rounded-full flex flex-1 justify-center items-center py-6 px-12 scale-95 transition duration-1000 ease-in-out hover:scale-100" htmlType="submit">
@@ -117,7 +129,6 @@ export function CadastroUsuario() {
                                 </div>
 
                             </Form>
-
                         </div>
 
                     </div>
